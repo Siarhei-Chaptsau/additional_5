8dc9b62fe39a2f48fb7e19bc1f91e7748396dd41
Siarhei <s.chaptsau@gmail.com>
2018-02-26 13:00:15 +0300

    Implemented function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  var arr = [];
+  arr = str.split(''); // создаём массив из строки
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (arr[i] === bracketsConfig[j][0] && arr[i + 1] === bracketsConfig[j][1]) { // проверка на соответствие правилу
+        arr.splice(i, 2); // удаляем 2 элемента соотвествующих правилу
+        i = -1;
+        break; // прерываем цикл и перебираем с начала (i = 0)
+      }
+    }
+  }
+
+  if (arr.length) {
+    return false;
+  } else {
+    return true;
+  }
 }
